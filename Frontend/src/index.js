1bcb937f7528c223e43b089a944073311475214a
Pongnawat Ratpitak <Pongnawat_R@snjinter.com>
2025-02-18 13:43:53 +0700

    My name is boss

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {APP} from './views/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <APP/>
   </React.StrictMode>
 );
---
+++ Frontend/src/views/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class APP extends React.Component{
+  render(){
+    return(
+      <App/>
+    )
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Boss</h1>
+      </div>
+    )
+  }
+}
